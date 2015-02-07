f2ea2162613ff1f09ea1edf4f920cd227cfea95e
vovacooper <vovacooper@gmail.com>
2015-02-07 15:52:11 +0200

    asd

--- geometa.js
+++ geometa.js
@@ -1,60 +1,60 @@
 // From
 // http://google-ajax-examples.googlecode.com/svn/trunk/whereareyou/scripts/geometa.js
 function prepareGeolocation(opt_force) {
-if ( opt_force || typeof navigator.geolocation == "undefined" || navigator.geolocation.shim ) (function(){
+    if ( opt_force || typeof navigator.geolocation == "undefined" || navigator.geolocation.shim ) (function(){
 
 // -- BEGIN GEARS_INIT
 (function() {
-  // We are already defined. Hooray!
-  if (window.google && google.gears) {
-    return;
-  }
-
-  var factory = null;
-
-  // Firefox
-  if (typeof GearsFactory != 'undefined') {
-    factory = new GearsFactory();
-  } else {
-    // IE
-    try {
-      factory = new ActiveXObject('Gears.Factory');
-      // privateSetGlobalObject is only required and supported on WinCE.
-      if (factory.getBuildInfo().indexOf('ie_mobile') != -1) {
-        factory.privateSetGlobalObject(this);
-      }
-    } catch (e) {
-      // Safari
-      if ((typeof navigator.mimeTypes != 'undefined')
-           && navigator.mimeTypes["application/x-googlegears"]) {
-        factory = document.createElement("object");
-        factory.style.display = "none";
-        factory.width = 0;
-        factory.height = 0;
-        factory.type = "application/x-googlegears";
-        document.documentElement.appendChild(factory);
-      }
+    // We are already defined. Hooray!
+    if (window.google && google.gears) {
+        return;
     }
-  }
 
-  // *Do not* define any objects if Gears is not installed. This mimics the
-  // behavior of Gears defining the objects in the future.
-  if (!factory) {
-    return;
-  }
+    var factory = null;
 
-  // Now set up the objects, being careful not to overwrite anything.
-  //
-  // Note: In Internet Explorer for Windows Mobile, you can't add properties to
-  // the window object. However, global objects are automatically added as
-  // properties of the window object in all browsers.
-  if (!window.google) {
-    google = {};
-  }
+    // Firefox
+    if (typeof GearsFactory != 'undefined') {
+        factory = new GearsFactory();
+    } else {
+        // IE
+        try {
+            factory = new ActiveXObject('Gears.Factory');
+            // privateSetGlobalObject is only required and supported on WinCE.
+            if (factory.getBuildInfo().indexOf('ie_mobile') != -1) {
+                factory.privateSetGlobalObject(this);
+            }
+        } catch (e) {
+            // Safari
+            if ((typeof navigator.mimeTypes != 'undefined')
+                && navigator.mimeTypes["application/x-googlegears"]) {
+                factory = document.createElement("object");
+                factory.style.display = "none";
+                factory.width = 0;
+                factory.height = 0;
+                factory.type = "application/x-googlegears";
+                document.documentElement.appendChild(factory);
+            }
+        }
+    }
 
-  if (!google.gears) {
-    google.gears = {factory: factory};
-  }
+    // *Do not* define any objects if Gears is not installed. This mimics the
+    // behavior of Gears defining the objects in the future.
+    if (!factory) {
+        return;
+    }
+
+    // Now set up the objects, being careful not to overwrite anything.
+    //
+    // Note: In Internet Explorer for Windows Mobile, you can't add properties to
+    // the window object. However, global objects are automatically added as
+    // properties of the window object in all browsers.
+    if (!window.google) {
+        google = {};
+    }
+
+    if (!google.gears) {
+        google.gears = {factory: factory};
+    }
 })();
 // -- END GEARS_INIT
 
